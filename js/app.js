e98a042aba500b8397d1bdae8dc6e7b52ec0c381
Dawid Spamer <dspamer@DS-8.local>
2014-12-08 16:31:32 -0800

    updating ...

--- js/app.js
+++ js/app.js
@@ -1,96 +1,36 @@
-﻿// Application module that injects routing, controller, and directive dependencies.
-var myApp = angular.module('myApp', ['ngRoute', 'myControllers', 'myDirectives'])
-                     .config(function ($routeProvider) {
-                         $routeProvider.when("/home", {
-                             // templateUrl: 'views/list.html',
-                             // controller: 'ListCtrl'
-                         }
-                    )
-             // If no route is selected then use the 'home' route.
-             .otherwise({ redirectTo: '/home' });
-                     });
+﻿// Declare module that references our controllers.
+var myApp = angular.module('myApp', ['ngRoute', 'cardAppControllers'])
+                   .config(function ($routeProvider) {
+                       $routeProvider.when("/home", {
+                           //templateUrl: 'views/main.html',
+                           //controller: 'AppCtrl'
+                       })
+                           // '/home/number/:numberID/suit/:suitID'
+                       .when("/aLink/num/:numID/str/:strID", {
+                           templateUrl: 'views/aView.html',
+                           controller: 'ACtrl'
+                       })
+                       // If no route is selected then use the 'home' route.
+                       .otherwise({ redirectTo: '/home' });
+                   });
 
-// Directive - Modifies HTML behaviour.
-var myDirectives = (function () {
-    var myDirectives = angular.module('myDirectives', []);
+var cardAppControllers = (function () {
+    var cardAppControllers = angular.module('cardAppControllers', []);
 
-    // directive() is a factory method to create directives.
-    myDirectives.directive('myStars', function () {
-        return {
-            restrict: 'A',
-            replace: true,
-            scope: {
-                rating: '=',
-                max: '='
-            },
-            link: function ($scope, elem, attrs, ctrl) {
-                var updateStars = function () {
-                    $scope.stars = [];
-                    for (var i = 0; i < $scope.max; i++) {
-                        if (i < $scope.rating)
-                            $scope.stars.push({ filled: true });
-                        else
-                            $scope.stars.push({ filled: false });
-                    }
-                };
-                // Here our watcher is bound to 'rating'.
-                $scope.$watch('rating', function () {
-                    updateStars();
-                });
-                $scope.toggle = function (index) {
-                    $scope.rating = index + 1;
-                };
-
-            },
-            templateUrl: function (element, attr) { return 'views/myStars.html' },
-        }
-    });
-    myDirectives.directive('myAlert', function () {
-        return {
-            restrict: 'E',
-            replace: false,
-            scope: {
-                //
-            },
-            link: function ($scope, elem, attrs, ctrl) {
-                $scope.alert = function (input) {
-                    alert( input );
-                };
-            },
-            templateUrl: function (element, attr) { return 'views/myAlert.html' },
-        }
-    });
-    myDirectives.directive('signUpForm', function () {
-        return {
-            restrict: 'E',
-            replace: false,
-            scope: {
-                //
-            },
-            link: function ($scope, elem, attrs, ctrl) {
-                $scope.alert = function (input) {
-                    alert( 'match' );
-                };
-
-                $scope.authenticate = function(value1, value2){
-                    return (!value1 && !value2) || !(value1 === value2);
-                };
-            },
-            templateUrl: function (element, attr) { return 'views/signUpForm.html' },
-        }
-    });
-
-
-    return myDirectives;
-}());
-
-// Controller - dispatches inputs and outputs.
-var myControllers = (function () {
-    var myControllers = angular.module('myControllers', []);
-
-    // Controllers are defined by the controller function.
-    myControllers.controller('AppCtrl', ['$scope', function ($scope) {
+    // Declare the application controller and inject the scope reference.
+    cardAppControllers.controller('AppCtrl', ['$scope', function ($scope) {
+        // Define the title model.
         $scope.title = "AngularJS Tutorial";
+        $scope.StartNum = 10;
+        $scope.StartString = "Hello!";
     }]);
-    return myControllers;
+    // Declare controller that populates 'list.html' with data and inject the scope.
+    cardAppControllers.controller('ACtrl', ['$scope', '$routeParams',
+                                             function ($scope, $routeParams) {
+                                                 // Define the cards model.
+                                                 $scope.subtitleA = "Detail A";
+                                                 $scope.myNum = $routeParams.numID;
+                                                 $scope.myString = $routeParams.strID;
+                                             }]);
+    return cardAppControllers;
 }());
